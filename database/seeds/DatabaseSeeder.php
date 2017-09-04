2bc41b8de7265c340c7544ee3d58dc16a2dd16e3
k3x4 <k3x4@DESKTOP-9N94VUB>
2017-09-04 03:55:03 +0300

    role::create bug remove

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(RolesTableSeeder::class);
-        //$this->call(PermissionsTableSeeder::class);
-        //$this->call(UsersTableSeeder::class);
-        //$this->call(RoleUserTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(RoleUserTableSeeder::class);
     }
 }
--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -11,32 +11,30 @@ class RolesTableSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        $roles = [
-            [
-                'name' => 'admin',
-                'display_name' => 'Administrator',
-                'description' => 'User has access to all system functionality.'
-            ],
-            [
-                'name' => 'editor',
-                'display_name' => 'Editor',
-                'description' => 'Control over all content on the site, but restricts the access to website settings.'
-            ],
-            [
-                'name' => 'author',
-                'display_name' => 'Author',
-                'description' => 'Manage only the content they have created.'
-            ],
-            [
-                'name' => 'user',
-                'display_name' => 'User',
-                'description' => 'Provides solely the ability to log in to the front end interface.'
-            ]
-        ];
-
-        foreach ($roles as $role) {
-            Role::create($role);
-        }
+        
+        $admin = new Role();
+        $admin->name = 'admin';
+        $admin->display_name = 'Administrator';
+        $admin->description = 'User has access to all system functionality.';
+        $admin->save();
+        
+        $editor = new Role();
+        $editor->name = 'editor';
+        $editor->display_name = 'Editor';
+        $editor->description = 'Control over all content on the site, but restricts the access to website settings.';
+        $editor->save();
+        
+        $author = new Role();
+        $author->name = 'author';
+        $author->display_name = 'Author';
+        $author->description = 'Manage only the content they have created.';
+        $author->save();        
+        
+        $user = new Role();
+        $user->name = 'user';
+        $user->display_name = 'User';
+        $user->description = 'Provides solely the ability to log in to the front end interface.';
+        $user->save();
         
     }
 
